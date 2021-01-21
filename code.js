096a835764d60e3d5000852f6a783af23db460e4
Tevin Banton <tevinanthony95@gmail.com>
2021-01-21 13:55:10 -0500

    points per game

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 module.exports = {
   consecutivePairs,
+  pointsPerGame,
 };
 
 //consecutive pairs - return the number of pairs that have consecutive numbers e.g: [1,2,5,8,-4,-3,7,6,5] = 3
@@ -17,4 +18,16 @@ function consecutivePairs(arr) {
   });
 
   return count;
+}
+
+/*Write a function that takes two arguments, ppg (points per game) and mpg (minutes per game) and returns 
+a straight extrapolation of ppg per 48 minutes rounded to the nearest tenth. Return 0 if 0.
+ @ppg - points per game
+ @mpg - minutes per game
+*/
+function pointsPerGame(ppg, mpg) {
+  let fullTime = 48;
+  if (mpg === 0) return 0;
+  let result = (ppg / mpg) * fullTime;
+  return +result.toFixed(1);
 }
--- code.test.js
+++ code.test.js
@@ -4,3 +4,8 @@ const functions = require('./code');
 test('Test for number of consecutive pairs', () => {
   expect(functions.consecutivePairs([1, 2, 5, 8, -4, -3, 7, 6, 5])).toBe(3);
 });
+
+//--------------------------------------Test for ooints per game-----------------------------------------
+test('Calculates the expected points per full game', () => {
+  expect(functions.pointsPerGame(30.8, 34.7)).toBe(42.6);
+});
